e982104dcc824db79f72c85e90907d576bbf570a
Emilie71 <martelemilie@hotmail.fr>
2019-04-18 11:46:55 +0200

    display maze

--- class_game/maze.py
+++ class_game/maze.py
@@ -3,49 +3,102 @@ from constants import *
 from random import sample
 
 
-class Map:
+class Maze:
 
     def __init__(self, filename):
         self.filename = filename
+        self.start = []
+        self.end = []
         self.paths = []
+        self.wall0 = []
+        self.wall1 = []
+        self.wall2 = []
+        self.wall3 = []
+        self.wall4 = []
+        self.wall5 = []
+        self.wall6 = []
+        self.wall7 = []
+        self.wall8 = []
+        self.wall9 = []
+        self.objects_to_find = []
 
     def is_path_position(self, position):
         return position in self.paths
 
     def load_from_file(self):
         """Fonction qui permet de charger la carte du labyrinthe"""
-        start = []
-        end = []
-        wall = []
-        structure = []
         with open(self.filename) as infile:
             for x, line in enumerate(infile):
                 for y, c in enumerate(line):
                     if c == path_char:
-                        self.paths.append(Position(x, y))
+                        self.paths.append(Position(y * size_sprite, x * size_sprite))
                     elif c == start_char:
-                        start.append(Position(x, y))
-                        self.paths.append(Position(x, y))
+                        self.start.append(Position(y * size_sprite, x * size_sprite))
+                        self.paths.append(Position(y * size_sprite, x * size_sprite))
                     elif c == end_char:
-                        end.append(Position(x, y))
-                        self.paths.append(Position(x, y))
-                    elif c == wall_char:
-                        wall.append(Position(x, y))
-        objects_to_find = sample(self.paths, 3)
-        print(objects_to_find)
-        structure.append(start)
-        structure.append(end)
-        structure.append(self.paths)
-        structure.append(wall)
+                        self.end.append(Position(y * size_sprite, x * size_sprite))
+                        self.paths.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '0':
+                        self.wall0.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '1':
+                        self.wall1.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '2':
+                        self.wall2.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '3':
+                        self.wall3.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '4':
+                        self.wall4.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '5':
+                        self.wall5.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '6':
+                        self.wall6.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '7':
+                        self.wall7.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '8':
+                        self.wall8.append(Position(y * size_sprite, x * size_sprite))
+                    elif c == '9':
+                        self.wall9.append(Position(y * size_sprite, x * size_sprite))
+        self.objects_to_find = sample(self.paths, 3)
 
-        print(structure)
-        return structure
+        return self.paths and self.wall0 and self.wall1 and self.wall2 and self.wall3 and self.wall4 and self.wall5 and self.wall6 and self.wall7 and self.wall8 and self.wall9 and self.objects_to_find and self.start and self.end
 
-
+    def display(self, window_name, start_name, end_name, keeper_name, wall0_name, wall1_name, wall2_name, wall3_name,
+                wall4_name, wall5_name, wall6_name, wall7_name, wall8_name, wall9_name, needle_name, tube_name,
+                ether_name):
+        for position in self.start:
+            window_name.blit(start_name, position.position)
+        for position in self.end:
+            window_name.blit(end_name, position.position)
+            window_name.blit(keeper_name, position.position)
+        for position in self.wall0:
+            window_name.blit(wall0_name, position.position)
+        for position in self.wall1:
+            window_name.blit(wall1_name, position.position)
+        for position in self.wall2:
+            window_name.blit(wall2_name, position.position)
+        for position in self.wall3:
+            window_name.blit(wall3_name, position.position)
+        for position in self.wall4:
+            window_name.blit(wall4_name, position.position)
+        for position in self.wall5:
+            window_name.blit(wall5_name, position.position)
+        for position in self.wall6:
+            window_name.blit(wall6_name, position.position)
+        for position in self.wall7:
+            window_name.blit(wall7_name, position.position)
+        for position in self.wall8:
+            window_name.blit(wall8_name, position.position)
+        for position in self.wall9:
+            window_name.blit(wall9_name, position.position)
+        objects = [needle_name, tube_name, ether_name]
+        x = 0
+        for position in self.objects_to_find:
+            window_name.blit(objects[x], position.position)
+            x += 1
 
 
 def main():
-    level = Map("level1")
+    level = Maze("level/level1")
 
     p = Position(0, 0).right().right().down()
     print(level.is_path_position(p))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
 from constants import *
+from class_game.maze import Maze
 
 
 # Pygame initialization
@@ -19,6 +20,33 @@ pygame.display.set_icon(icon)
 # 3- Window title
 pygame.display.set_caption(title_window)
 
+# Load image for game (crop and resizing)
+start = pygame.transform.scale((pygame.image.load(floor_tiles)).subsurface(160, 20, 20, 20), (30, 30))
+end = pygame.transform.scale((pygame.image.load(floor_tiles)).subsurface(220, 20, 20, 20), (30, 30))
+path = pygame.transform.scale(pygame.image.load(floor_tiles).convert().subsurface(60, 60, 20, 20), (30, 30))
+wall0 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(40, 20, 20, 20), (30, 30))
+wall1 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(20, 40, 20, 20), (30, 30))
+wall2 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(20, 20, 20, 20), (30, 30))
+wall3 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(20, 80, 20, 20), (30, 30))
+wall4 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(80, 20, 20, 20), (30, 30))
+wall5 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(80, 80, 20, 20), (30, 30))
+wall6 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(210, 0, 20, 20), (30, 30))
+wall7 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(210, 20, 20, 20), (30, 30))
+wall8 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(220, 10, 20, 20), (30, 30))
+wall9 = pygame.transform.scale(pygame.image.load(structure_wall).convert().subsurface(200, 10, 20, 20), (30, 30))
+hero = pygame.transform.scale(pygame.image.load(hero_source).convert_alpha().subsurface(0, 0, 32, 32), (30, 30))
+keeper = pygame.transform.scale(pygame.image.load(keeper_image).convert_alpha(), (25, 25))
+needle = pygame.transform.scale(pygame.image.load(needle_image).convert(), (23, 30))
+tube = pygame.transform.scale(pygame.image.load(tube_image).convert_alpha(), (30, 23))
+ether = pygame.transform.scale(pygame.image.load(ether_image).convert(), (30, 30))
+syringe = pygame.transform.scale(pygame.image.load(syringe_image).convert_alpha(), (30, 30))
+
+# Create maze
+level = Maze("level/level1")
+level.load_from_file()
+position_hero = level.start
+position_guardian = level.end
+
 # Variable that continues the loop if = 1, stops if = 0
 main_loop = 1
 
@@ -28,3 +56,9 @@ while main_loop:
         # If user quit the program stop or if the user presses a ESCAPE key
         if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
             main_loop = 0
+
+    level.display(window, start, end, keeper, wall0, wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9,
+                  needle, tube, ether)
+
+    pygame.display.flip()
+
